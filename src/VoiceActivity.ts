8944701ab0c4e7135809848a05e485c4af680c1f
theteachr <sop.theteachr@gmail.com>
2022-09-13 03:33:20 +0530

    Improve comments

--- src/VoiceActivity.ts
+++ src/VoiceActivity.ts
@@ -48,16 +48,19 @@ class VoiceActivity {
 			return null
 		}
 
-		// Return the number of seconds that passed from the time the
-		// activity was started to now.
+		// Get the current time.
 		const currentTime = Date.now()
+		// Get the number of seconds that have passed from when `startTime` was set.
+		const seconds = (currentTime - this.startTime) / 1000
 
-		console.log(`Current Time: ${currentTime}`)
-		console.log(`Voice Activity Start Time: ${this.startTime}`)
+		// TODO: Wait for a specific amount of time before actually stopping to
+		// track the activity to account for temporary disconnects and other similar
+		// scenarios.
+		// (This should probably be handled in a different place.)
 
 		this.state.stop()
 
-		return (currentTime - this.startTime) / 1000
+		return seconds
 	}
 }
 
